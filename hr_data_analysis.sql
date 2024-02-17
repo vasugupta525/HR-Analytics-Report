a7f2ea003d76c6d1f95f0ed654cfa2bfcc860d90
Vasu Gupta <72926408+vasugupta525@users.noreply.github.com>
2024-02-17 17:37:05 +0530

    Add files via upload

--- hr_data_analysis.sql
+++ hr_data_analysis.sql
@@ -1,63 +1,66 @@
 use hr_dataset;
 
-select * from hr_data;
+SELECT * FROM hr_data;
 
-select count(*) from hr_data;
+SELECT COUNT(*) FROM hr_data;
 
 -- Employee Count:
-select sum(employee_count) as Employee_Count from hr_data;
+SELECT SUM(employee_count) AS Employee_Count FROM hr_data;
 
 
 -- Attrition Count:
-select count(attrition) as attrition_count from hr_data where attrition='Yes';
+SELECT COUNT(attrition) AS attrition_count FROM hr_data
+WHERE attrition = 'Yes';
 
 
 -- Attrition Rate:
-select round (((select count(attrition) from hr_data where attrition='Yes')/ 
-sum(employee_count)) * 100,2) as attrition_rate
-from hr_data;
+SELECT ROUND(((SELECT COUNT(attrition) FROM hr_data
+WHERE attrition = 'Yes') / SUM(employee_count)) * 100, 2) AS attrition_rate
+FROM hr_data;
 
 
 -- Active Employee:
-select sum(employee_count) - (select count(attrition) from hr_data  where attrition='Yes') as active_employee from hr_data;
+SELECT SUM(employee_count) - (SELECT COUNT(attrition) FROM hr_data 
+WHERE attrition = 'Yes') AS active_employee
+FROM hr_data;
 -- OR 
-select (select sum(employee_count) from hr_data) - count(attrition) as active_employee from hr_data
-where attrition='Yes';
+SELECT (SELECT SUM(employee_count) FROM hr_data) - COUNT(attrition) AS active_employee FROM hr_data
+WHERE attrition = 'Yes';
 
 
 -- Average Age:
-select round(avg(age),0) as average_age from hr_data;
+SELECT ROUND(AVG(age), 0) AS average_age FROM hr_data;
 
 
 -- Attrition by Gender:
-select gender, count(attrition) as attrition_count from hr_data
-where attrition='Yes'
-group by gender
-order by count(attrition) desc;
+SELECT gender, COUNT(attrition) AS attrition_count FROM hr_data
+WHERE attrition = 'Yes'
+GROUP BY gender
+ORDER BY COUNT(attrition) DESC;
 
 
 -- Department wise Attrition:
-select department, count(attrition) as attrition_count from hr_data
-where attrition='Yes'
-group by department 
-order by count(attrition) desc;
+SELECT department, COUNT(attrition) AS attrition_count FROM hr_data
+WHERE attrition = 'Yes'
+GROUP BY department
+ORDER BY COUNT(attrition) DESC;
 
 
 -- No of Employee by Age Group:
-SELECT age,  sum(employee_count) AS employee_count FROM hr_data
+SELECT age, SUM(employee_count) AS employee_count FROM hr_data
 GROUP BY age
-order by age;
+ORDER BY age;
 
 
 -- Education Field wise Attrition:
-select education_field, count(attrition) as attrition_count from hr_data
-where attrition='Yes'
-group by education_field
-order by count(attrition) desc;
+SELECT education_field, COUNT(attrition) AS attrition_count FROM hr_data
+WHERE attrition = 'Yes'
+GROUP BY education_field
+ORDER BY COUNT(attrition) DESC;
 
 
 -- Attrition Rate by Gender for different Age Group:
-select `CF_age band`, gender, count(attrition) as attrition from hr_data
-where attrition = 'Yes'
-group by `CF_age band`, gender
-order by `CF_age band`, gender desc;
+SELECT `CF_age band`, gender, COUNT(attrition) AS attrition FROM hr_data
+WHERE attrition = 'Yes'
+GROUP BY `CF_age band` , gender
+ORDER BY `CF_age band` , gender DESC;
